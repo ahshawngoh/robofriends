1f5ca58f28296f02e30a0c3cdab62da814cd4575
unknown <ahshawngoh@gmail.com>
2022-04-21 20:42:36 +0800

    Updated app with Hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,41 +1,38 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll'
 import ErrorBoundary from '../components/ErrorBoundary';
 import './App.css';
 
-class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            robots: [],
-            searchfield: ''
-        }
-    }
+function App() {
+    const [robots, setRobots] = useState( [] )
+    const [searchfield, setSearchfield] = useState('')
+    const [count, setCount] = useState(0)
 
-    componentDidMount() {
+    useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(response => response.json())
-            .then(users => {this.setState({robots: users})});
-    }
+            .then(users => setRobots(users));
+        console.log(count)
+    },[count]) // only run when count changes, empty [] when similar to componentDidMount
 
-    onSearchChange = (event) => {
-        this.setState({ searchfield: event.target.value })
+    const onSearchChange = (event) => {
+        setSearchfield(event.target.value)
     }
     
-    render() {
-        const { robots, searchfield } = this.state;
-        const filteredRobots = robots.filter(robot => {
-            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-        })
-        return !robots.length ?
+    const filteredRobots = robots.filter(robot => {
+        return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+    })
+
+    return !robots.length ?
         <h1>Loading</h1> : 
         (
             <div className='tc'>
                 <h1 className='f1'>RoboFriends</h1>
+                <button onClick={()=>setCount(count+1)}>Click me!</button>
                 <React.StrictMode>
-                    <SearchBox searchChange={this.onSearchChange}/>
+                    <SearchBox searchChange={onSearchChange}/>
                     <Scroll>
                         <ErrorBoundary>
                             <CardList robots={filteredRobots}/>
@@ -44,7 +41,6 @@ class App extends Component {
                 </React.StrictMode>
             </div>
         )
-    }
 }
 
 export default App
